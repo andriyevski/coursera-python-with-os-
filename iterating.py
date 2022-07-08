e08145244458d69f244668b157f95a7097ea31fe
Andriyevski <andriyevski@gmail.com>
2022-07-08 18:37:53 +0300

    We make 2 function to same task!

--- iterating.py
+++ iterating.py
@@ -1,3 +1,14 @@
-with open("list.csv") as file:
-    for line in file:
-        print(line.upper())
+def standart():
+    with open("list.csv") as file:
+        for line in file:
+            print(line.upper())
+
+def another():
+    file = open("list.csv")
+    lines = file.readlines()
+    file.close()
+    lines.sort()
+    print(lines)
+
+standart()
+another()
